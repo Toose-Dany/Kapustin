8ee7b9b98e14a06ccbf524f6d4a29f5ee993e714
Daniil.K <danyfaik228@gmail.com>
2025-09-16 10:03:53 +0300

    FlightAPI

--- service.py
+++ service.py
@@ -1,103 +1,28 @@
-import sqlite3
+from repository import FlightRepository
 from flight import Flight
 
-import psycopg2
-from psycopg2 import sql
-
 class FlightService:
-    def __init__(self,
-                 database: str = "", 
-                 host: str="", 
-                 user:str = "", 
-                 password:str = "",
-                 port:str=""):
-        self.connection_parameters = {
-            'host':host,
-            'database':database,
-            'user':user,
-            'password':password,
-            'port':port
-        }
-        self.init_db()
+    def __init__(self,repository:FlightRepository):
+        self.repository = repository
 
-    def get_connection(self):
-        return psycopg2.connect(**self.connection_parameters)
-    
-    def init_db(self):
-        """Иницилизация таблиц"""
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS flights(
-                         id SERIAL PRIMARY KEY,
-                         plane VARCHAR(100) NOT NULL,
-                         price DECIMAL(10,2) NOT NULL
-                         )
-                ''')
-            conn.commit()
-            
     def create_flight(self, flight:Flight):
         """Добавление рейса"""
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                INSERT INTO flights
-                         (plane,price)
-                         VALUES (%s,%s)
-                ''',(flight.plane,flight.price))
-            conn.commit()
-            return cursor.rowcount > 0
+        return self.repository.create_flight(flight)
     
     def get_all(self):
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM flights ORDER BY id")
-            rows = cursor.fetchall()
-
-            flights = []
-            for row in rows:
-                flights.append(Flight(
-                    row[0],
-                    row[1],
-                    row[2]
-                ))
-            return flights
+        '''Получить все полёты'''
+        return self.repository.get_all()
         
     def get_by_id(self,flight_id:int):
-        """Получить рейс по идентификатору"""
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM flights WHERE id = %s",(flight_id,))
-            row = cursor.fetchone()
-
-            if row:
-                return Flight(
-                    row[0],
-                    row[1],
-                    row[2]
-                )
-        return None
+        '''Получить полёт по id'''
+        return self.repository.get_by_id(flight_id)
     
     def update_flight(self, flight:Flight):
         """Изменить существующий рейс. 
             Если рейса не существует, ничего не делать."""
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                UPDATE flights
-                SET price = %s, plane = %s
-                WHERE id = %s
-                ''',(flight.price, flight.plane, flight.id))
-            conn.commit()
-            return cursor.rowcount > 0
+        return self.repository.update_flight(flight)
     
     def delete_flight(self,flight_id:int):
         """Удалить существующий рейс.
             Если рейса не существует, ничего не делать."""
-        with self.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                DELETE FROM flights WHERE id = %s
-                ''',(flight_id,))
-            conn.commit()
-            return cursor.rowcount > 0
+        return self.repository.delete_flight(flight_id)
